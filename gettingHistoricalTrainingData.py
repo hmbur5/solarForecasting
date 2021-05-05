168b5a1384c6714b1c9cbf064fd65ed6a658df41
Hannah Burke <hmbur5@student.monash.edu>
2021-05-05 17:50:36 +1000

    expanding model for different solar panels by using 3 days of historical data as the first later

--- gettingHistoricalTrainingData.py
+++ gettingHistoricalTrainingData.py
@@ -1,119 +1,129 @@
-# to do
-# find way to turn postcodes into location strings (API?) - done
-# implement locName variable in relevant locations - done
-# selecting the correct location name from postcodes with multiple names - done, could be better
-# pulling postcodes from large data sets
-# expanding to be able to do multiple customers
-
 import csv
 import pandas as pd
 import datetime
-import pgeocode as pg 
-## Using pgeocode to translate postcodes to name strings:
-# nomi = pg.Nominatim('AU')
-# frame = nomi.query_postal_code("3168")
-# namestring = frame.place_name  
-#
-# installing: pip install pgeocode
-
+import pgeocode as pg
+from WorldWeatherPy import HistoricalLocationWeather
+# weather data from https://www.worldweatheronline.com/developer/api/docs/historical-weather-api.aspx
+# use api to download historical weather data from the same place and time
+api_key = 'f2f090e1b01d4d7ea1435335211404'
 
 #pgeocode global control, setting AU database
 nomi = pg.Nominatim('AU')
 
 # solar data downloaded from https://www.ausgrid.com.au/Industry/Our-Research/Data-to-share/Solar-home-electricity-data
 # opening solar data file, and getting data for customer 1
-with open('Desktop\Monash\\0. 2021\\1. Sem 1\TRC4200\Group Project\Repo\solarForecasting\data\\2012-2013 Solar home electricity data v2.csv', "r") as csv_file: #change back to: data\2012-2013 Solar home electricity data v2.csv
+customerDict = {}
+with open('data/2012-2013 Solar home electricity data v2.csv', "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    customer1 = []
     for lines in csv_reader:
-        # limit to first customer
         if lines[0] == 'Customer':
             header = lines
-        if lines[0]=='1':
-            # just add generation data
-            if lines[3]=='GG':
-                customer1.append(lines)
-        #will need to expand out from here for more customers
+        else:
+            # only using generation data
+            if lines[3] == 'GG':
+                try:
+                    customerDict[lines[0]].append(lines)
+                except KeyError:
+                    customerDict[lines[0]] = [lines]
 
-# convert to dataframe
-customer1Data = pd.DataFrame(data=customer1, columns=header)
-#print(customer1[1][2]) change this to pandas data frame, was having issues so used this as proof of concept
+for customer in customerDict.keys():
+    customerList = customerDict[customer]
 
-#Reading postcode data, creating data frame and isolating location name
-#locPostcode = customer1data.loc[1,"Postcode"] # semihardcoded for customer 1, will need to introduce variation - see above
-locPostcode = customer1[1][2] #not the best method, see print line comment
-locData = nomi.query_postal_code(locPostcode) 
-locName = locData.place_name
-print(locName)
+    # convert to dataframe
+    customerData = pd.DataFrame(data=customerList, columns=header)
+    #print(customerList[1][2]) change this to pandas data frame, was having issues so used this as proof of concept
 
-# splitting in case of multiple locations in same postcode
-chunks = locName.split(',')
-locName = chunks[0]
-print(locName)
+    #Reading postcode data, creating data frame and isolating location name
+    #locPostcode = customerdata.loc[1,"Postcode"] # semihardcoded for customer 1, will need to introduce variation - see above
+    locPostcode = customerList[1][2] #not the best method, see print line comment
+    locData = nomi.query_postal_code(locPostcode)
+    locName = locData.place_name
 
-# convert generation data to hourly rather than half hourly (so it matches with weather)
-for i in range(24):
-    half_hour = str(i)+':30'
-    if i+1==24:
-        hour = '0:00'
-    else:
-        hour = str(i+1)+':00'
-    customer1Data[half_hour] = pd.to_numeric(customer1Data[half_hour],downcast="float")
-    customer1Data[hour] = pd.to_numeric(customer1Data[hour],downcast="float")
-    customer1Data[hour] = customer1Data[half_hour] + customer1Data[hour]
-    del customer1Data[half_hour]
-
-# convert to rows corresponding to hours rather than columns (so it matches with weather)
-row_data = pd.DataFrame([], columns = ['date_time', 'power generation kwh'])
-for index, row in customer1Data.iterrows():
-    # iterating through hours (starting at 1, then to midnight
-    for i in list(range(1,24))+[0]:
-        hour = str(i) + ':00'
-        date = customer1Data.at[index, 'date']
-        date_time_obj = datetime.datetime.strptime(date+' '+hour, '%d/%m/%Y %H:%M')
-        # if it gets to midnight, switch to next day (based on convention used in weather data)
-        if hour == '0:00':
-            date_time_obj = date_time_obj+datetime.timedelta(days=1)
-        date_time_str = date_time_obj.strftime('%Y-%m-%d %H:%M:%S')
-
-        generation = customer1Data.at[index, hour]
-
-        appending_dataframe = pd.DataFrame([[date_time_str, generation]], columns = ['date_time', 'power generation kwh'])
-
-        row_data = row_data.append(appending_dataframe)
+    # splitting in case of multiple locations in same postcode
+    chunks = locName.split(', ')
+    done=False
+    for chunk in chunks:
+        locName = chunk
+        # use api to download historical weather data from the same place and time
+        city = locName.replace(' ', '+')+'+Australia'
+        start_date = '2012-07-01'
+        end_date = '2013-6-30'
+        frequency = 1  # hourly frequency
+        # skip over locations that have already been done
+        try:
+            with open('data/simplified ' + locName + ' weather.csv', "r") as csv_file:
+                done=True
+                break
+        except FileNotFoundError:
+            pass
+        try:
+            dataset = HistoricalLocationWeather(api_key, city, start_date, end_date, frequency).retrieve_hist_data()
+        except KeyError:
+            print(locName)
+        else:
+            break
+    if done==True:
+        continue
 
 
-# save this data
-row_data.to_csv('Desktop\Monash\\0. 2021\\1. Sem 1\TRC4200\Group Project\Repo\solarForecasting\data\simplified Wahroonga solar.csv',index=False) #change back to: data\simplified Wahroonga solar.csv
+    print(locName)
+
+    # convert generation data to hourly rather than half hourly (so it matches with weather)
+    for i in range(24):
+        half_hour = str(i)+':30'
+        if i+1==24:
+            hour = '0:00'
+        else:
+            hour = str(i+1)+':00'
+        customerData[half_hour] = pd.to_numeric(customerData[half_hour],downcast="float")
+        customerData[hour] = pd.to_numeric(customerData[hour],downcast="float")
+        customerData[hour] = customerData[half_hour] + customerData[hour]
+        del customerData[half_hour]
+
+    # convert to rows corresponding to hours rather than columns (so it matches with weather)
+    row_data = pd.DataFrame([], columns = ['date_time', 'power generation kwh'])
+    for index, row in customerData.iterrows():
+        # iterating through hours (starting at 1, then to midnight
+        for i in list(range(1,24))+[0]:
+            hour = str(i) + ':00'
+            date = customerData.at[index, 'date']
+            date_time_obj = datetime.datetime.strptime(date+' '+hour, '%d/%m/%Y %H:%M')
+            # if it gets to midnight, switch to next day (based on convention used in weather data)
+            if hour == '0:00':
+                date_time_obj = date_time_obj+datetime.timedelta(days=1)
+            date_time_str = date_time_obj.strftime('%Y-%m-%d %H:%M:%S')
+
+            generation = customerData.at[index, hour]
+
+            appending_dataframe = pd.DataFrame([[date_time_str, generation]], columns = ['date_time', 'power generation kwh'])
+
+            row_data = row_data.append(appending_dataframe)
+
+
+    # save this data
+    row_data.to_csv('data/simplified '+locName+' solar.csv',index=False)
 
 
 
 
 
-# weather data from https://www.worldweatheronline.com/developer/api/docs/historical-weather-api.aspx
-# use api to download historical weather data from the same place and time
-from WorldWeatherPy import HistoricalLocationWeather
-api_key = 'f2f090e1b01d4d7ea1435335211404'
-city = locName
-start_date = '2012-07-01'
-end_date = '2013-6-30'
-frequency = 1 # hourly frequency
-dataset = HistoricalLocationWeather(api_key, city, start_date, end_date, frequency).retrieve_hist_data()
 
 
-# deleting some data for simplicity, particularly stuff that doesn't appear in forecasting
-del dataset['maxtempC']
-del dataset['mintempC']
-del dataset['totalSnow_cm']
-del dataset['sunHour']
-del dataset['moon_illumination']
-del dataset['moonrise']
-del dataset['moonset']
-del dataset['DewPointC']
-del dataset['FeelsLikeC']
-del dataset['HeatIndexC']
-del dataset['WindChillC']
-del dataset['WindGustKmph']
 
-# save data
-dataset.to_csv('Desktop\Monash\\0. 2021\\1. Sem 1\TRC4200\Group Project\Repo\solarForecasting\data\simplified Wahroonga weather.csv') #swap back to: data\simplified Wahroonga weather.csv
+
+    # deleting some data for simplicity, particularly stuff that doesn't appear in forecasting
+    del dataset['maxtempC']
+    del dataset['mintempC']
+    del dataset['totalSnow_cm']
+    del dataset['sunHour']
+    del dataset['moon_illumination']
+    del dataset['moonrise']
+    del dataset['moonset']
+    del dataset['DewPointC']
+    del dataset['FeelsLikeC']
+    del dataset['HeatIndexC']
+    del dataset['WindChillC']
+    del dataset['WindGustKmph']
+
+    # save data
+    dataset.to_csv('data/simplified '+ locName +' weather.csv')
--- lstm.py
+++ lstm.py
@@ -5,103 +5,200 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import random
 from datetime import datetime
+import pandas as pd
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from matplotlib import pyplot as plt
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import LSTM
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed, Dropout, MaxPooling1D, AveragePooling1D
+from keras import regularizers
+from keras import Input
+from keras import Model
+from keras.layers import Concatenate
+from keras.optimizers import adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TerminateOnNaN
+from keras import models
+import pickle
+import random
+from math import floor, ceil
+from keras.initializers import Orthogonal
+import os
+
+# get all csv files
+directory = 'data/'
+locations = []
+for filename in os.listdir(directory):
+    if filename.endswith("weather.csv"):
+        filename = filename.replace('simplified ', '')
+        locations.append(filename.replace(' weather.csv',''))
+    else:
+        continue
+
+first_input_sequences = np.empty((0,72,9))
+second_input_sequences = np.empty((0,24,8))
+output_sequences = np.empty((0,24,1))
+days_sequences = []
+
+for location in locations[:10]:
+
+    # splitting data up into days so we can simply predict the solar generation on a given day
+    days = []
+    with open("data/simplified "+location+" solar.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
+            if day not in days:
+                days.append(day)
+
+    # get input from weather data
+    input_sequence = np.empty((len(days),24,8), dtype=np.float64)
+    with open("data/simplified "+location+" weather.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            current_day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
+            try:
+                index = days.index(current_day)
+            except ValueError:
+                continue
+            hour = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').hour
+            # using uvIndex,cloudcover,humidity,precipMM,pressure,tempC,visibility
+            for index1, index2 in enumerate([1,4,5,6,7,8,9]):
+                input_sequence[index, hour - 1,index1] = np.float64(row[index2])
+            # adding boolean for whether sun is up
+            currentTime = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').time()
+            sunrise = datetime.strptime(row[2], '%I:%M %p').time()
+            sunset = datetime.strptime(row[3], '%I:%M %p').time()
+            if currentTime>sunrise and currentTime<sunset:
+                input_sequence[index, hour - 1, 7] =1
+            else:
+                input_sequence[index, hour - 1, 7] = 0
 
 
+    output_sequence = np.empty((len(days),24,1), dtype=np.float64)
+    with open("data/simplified "+location+" solar.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            current_day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
+            index = days.index(current_day)
+            hour = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').hour
+            output_sequence[index,hour-1,0]=np.float64(row[1])
 
-# splitting the data up into days so we can simply predict the solar generation on a given day
-days = []
-with open("data/simplified Wahroonga solar.csv", 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    next(reader)
-    for row in reader:
-        day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
-        if day not in days:
-            days.append(day)
+    # skip first and last days for now as the midnight is missing from them
+    input_sequence = input_sequence[1:-1]
+    output_sequence = output_sequence[1:-1]
+    days = days[1:-1]
 
-# get input from weather data
-input_sequence = np.empty((len(days),24,8), dtype=np.float64)
-with open("data/simplified Wahroonga weather.csv", 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    next(reader)
-    for row in reader:
-        current_day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
-        index = days.index(current_day)
-        hour = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').hour
-        # using uvIndex,cloudcover,humidity,precipMM,pressure,tempC,visibility
-        for index1, index2 in enumerate([1,4,5,6,7,8,9]):
-            input_sequence[index, hour - 1,index1] = np.float64(row[index2])
-        # adding boolean for whether sun is up
-        currentTime = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').time()
-        sunrise = datetime.strptime(row[2], '%I:%M %p').time()
-        sunset = datetime.strptime(row[3], '%I:%M %p').time()
-        if currentTime>sunrise and currentTime<sunset:
-            input_sequence[index, hour - 1, 7] =1
-        else:
-            input_sequence[index, hour - 1, 7] = 0
+    # shuffle so summer/winter data is mixed up (or validation/testing will all be same type of data)
+    indices = list(range(len(days)))
+    random.shuffle(indices)
+    input_sequence = input_sequence[indices]
+    output_sequence = output_sequence[indices]
+    days = np.array(days)[indices]
+
+    # first input sequences is an array of weather data and solar output for 3 random days
+    example_data = np.empty((1,72, input_sequence.shape[-1]+1))
+    remaining_indices = list(range(len(input_sequence)))
+    example_days = random.sample(range(len(input_sequence)),3)
+    for index,day in enumerate(example_days):
+        for hour in range(0,24):
+            example_data[0,index*hour+hour]=np.append(input_sequence[day][hour],output_sequence[day][hour])
+        remaining_indices.remove(day)
+    # add exampple data as an element to first input sequence for each of the remaining days
+    for _ in range(len(remaining_indices)):
+        first_input_sequences = np.append(first_input_sequences, example_data, axis=0)
+
+    input_sequence = np.array(input_sequence)[remaining_indices]
+    output_sequence = np.array(output_sequence)[remaining_indices]
+    days = np.array(days)[remaining_indices]
 
 
-output_sequence = np.empty((len(days),24,1), dtype=np.float64)
-with open("data/simplified Wahroonga solar.csv", 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    next(reader)
-    for row in reader:
-        current_day = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').date()
-        index = days.index(current_day)
-        hour = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').hour
-        output_sequence[index,hour-1,0]=np.float64(row[1])
+    second_input_sequences = np.append(second_input_sequences, input_sequence, axis=0)
+    output_sequences = np.append(output_sequences, output_sequence, axis=0)
+    day_customer = []
+    for day in days:
+        day_customer.append(str(day)+' '+ location)
+    days_sequences+=day_customer
+    print(location)
 
-# skip first and last days for now as the midnight is missing from them
-input_sequence = input_sequence[1:-1]
-output_sequence = output_sequence[1:-1]
-days = days[1:-1]
 
-# shuffle so summer/winter data is mixed up (or validation/testing will all be same type of data)
-indices = list(range(len(days)))
+# shuffle so solar panel data is mixed up (or validation/testing will all be same type of data)
+indices = list(range(len(days_sequences)))
 random.shuffle(indices)
-input_sequence = input_sequence[indices]
-output_sequence = output_sequence[indices]
-days = np.array(days)[indices]
+first_input_sequences = first_input_sequences[indices]
+second_input_sequences = second_input_sequences[indices]
+output_sequences = output_sequences[indices]
+days_sequences = np.array(days_sequences)[indices]
+print(np.shape(first_input_sequences))
+print(np.shape(second_input_sequences))
+print(np.shape(output_sequences))
+print(np.shape(days_sequences))
 
 
 # normalise (should only normalise in terms of the train data, but leaving for now)
 scaler = MinMaxScaler(feature_range=(0, 1))
-for i in range(input_sequence.shape[1]):
-    scaler = scaler.fit(input_sequence[:, i, :])
-    input_sequence[:, i, :] = scaler.transform(input_sequence[:, i, :])
+for i in range(first_input_sequences.shape[1]):
+    scaler = scaler.fit(first_input_sequences[:, i, :])
+    first_input_sequences[:, i, :] = scaler.transform(first_input_sequences[:, i, :])
+scaler1 = MinMaxScaler(feature_range=(0, 1))
+for i in range(second_input_sequences.shape[1]):
+    scaler1 = scaler1.fit(second_input_sequences[:, i, :])
+    second_input_sequences[:, i, :] = scaler1.transform(second_input_sequences[:, i, :])
 scaler2 = MinMaxScaler(feature_range=(0, 1))
-for i in range(output_sequence.shape[1]):
-    scaler2 = scaler2.fit(output_sequence[:, i, :])
-    output_sequence[:, i, :] = scaler2.transform(output_sequence[:, i, :])
+for i in range(output_sequences.shape[1]):
+    scaler2 = scaler2.fit(output_sequences[:, i, :])
+    output_sequences[:, i, :] = scaler2.transform(output_sequences[:, i, :])
 
 
 
 # split days into training and testing
 training_portion = np.float64(0.8)
-train_size = int(len(days) * training_portion)
-train_input = input_sequence[0: train_size]
-train_output = output_sequence[0: train_size]
-validation_input = input_sequence[train_size:-4]
-validation_output = output_sequence[train_size:-4]
-test_input = input_sequence[-4:]
-test_output = output_sequence[-4:]
-test_days = days[-4:]
+train_size = int(len(days_sequences) * training_portion)
+train_input_historic = first_input_sequences[0: train_size]
+train_input_weather = second_input_sequences[0: train_size]
+train_output = output_sequences[0: train_size]
+validation_input_historic = first_input_sequences[train_size:-4]
+validation_input_weather = second_input_sequences[train_size:-4]
+validation_output = output_sequences[train_size:-4]
+test_input_historic = first_input_sequences[-4:]
+test_input_weather = second_input_sequences[-4:]
+test_output = output_sequences[-4:]
+test_days = days_sequences[-4:]
 
-
+print(np.shape(train_input_historic))
 
 # define model
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(200, input_shape=(train_input.shape[1],train_input.shape[2]), return_sequences=True))
-model.add(tf.keras.layers.Dense(100, activation='relu'))
-model.add(tf.keras.layers.Dense(train_output.shape[2]))
+encoder_input = Input(shape=(72,9))
+forecast_input = Input(shape=(24,8))
+encoder_layer_1 = LSTM(200, activation='relu', kernel_initializer=Orthogonal())
+encoder_hidden_output = encoder_layer_1(encoder_input)
+decoder_input = RepeatVector(24)(encoder_hidden_output)
+decoder_input = Dropout(0.2)(decoder_input)
+decoder_input = Concatenate(axis=2)([decoder_input, forecast_input])
+decoder_layer = LSTM(200, activation='relu', return_sequences=True, kernel_initializer=Orthogonal())
+decoder_output = decoder_layer(decoder_input)
+dense_input = Dropout(0.2)(decoder_output)
+dense_layer = TimeDistributed(Dense(100, activation='relu'))
+dense_output = dense_layer(dense_input)
+outputs = TimeDistributed(Dense(1))(dense_output)
+model = Model(inputs=[encoder_input, forecast_input], outputs=outputs, name="model")
+print(model.summary)
 
 model.compile(loss='mse', optimizer='adam')
-num_epochs = 100
-history = model.fit(train_input, train_output, epochs=num_epochs, validation_data=(validation_input, validation_output), verbose=2)
+num_epochs = 50
+history = model.fit([train_input_historic, train_input_weather], train_output, epochs=num_epochs, validation_data=([validation_input_historic, validation_input_weather], validation_output), verbose=2)
 
 
 
 # using testing data to plot some days
-prediction = model.predict(test_input)
+prediction = model.predict([test_input_historic, test_input_weather])
 observation = test_output
 
 
